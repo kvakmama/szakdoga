89c27927e78b6376337b7c3762207fa9370cc4d7
kvakmama <kvakmama@gmail.com>
2019-03-24 20:53:02 +0100

    4.fejezet: interface

---
+++ juci/juci/Osszehasonlito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juci
+{
+    public class Osszehasonlito<T> where T:IComparable
+    {
+        public bool NagyobbAzElsoMintAMasodik(T elso, T masodik)
+        {
+            return elso.CompareTo(masodik) >= 0;
+        }
+    }
+}
---
+++ juci/juci/Tank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juci
+{
+    public class Tank: ISebezheto
+    {
+        int eletero;
+        public void Sebzodj(int mennyit)
+        {
+            eletero = eletero - mennyit;
+        }
+    }
+}
