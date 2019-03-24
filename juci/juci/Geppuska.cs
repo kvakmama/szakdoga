8d6167eaf6b80b57b2eeaeaad9cbc04aea515c88
kvakmama <kvakmama@gmail.com>
2019-03-24 19:26:42 +0100

    3.fejezet: generic new class

---
+++ juci/juci/Geppuska.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juci
+{
+    class Geppuska : Fegyver
+    {
+        public override void Lo()
+        {
+            Console.WriteLine("asadada");
+        }
+    }
+}
